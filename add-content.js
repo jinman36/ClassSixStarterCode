5e735b12e6add5be873661497ac3833abefe24fe
Jeff Inman <jeffery.inman@hotmail.com>
2020-12-13 19:31:41 -0800

    re-wrote the starter code

--- add-content.js
+++ add-content.js
@@ -1,15 +1,15 @@
 var today = new Date ();
 var hourNow = today.getHours();
-var greetings;
+var greeting;
 
 if (hourNow > 18) {
-    greetings - 'Salamat Malam!';
+    greeting - 'Salamat Malam!';
 } else if (hourNow > 12) {
-    greetings = 'Selamat Sore!';
+    greeting = 'Selamat Sore!';
 } else if (hourNow > 0) {
-    greetings = 'Selamat Pagi!';
+    greeting = 'Selamat Pagi!';
 } else {
-    greetings = 'Selamat!';
+    greeting = 'Selamat!';
 }
 
-document.write('<h3>' + greetings + '</h3>');
+document.write('<h3>' + greeting + '</h3>');
